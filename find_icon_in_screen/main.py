f487417e4f92b8b7e06c0fe6c7643cc13d2ed874
Neto Costa <54679690+netocosta@users.noreply.github.com>
2021-12-30 15:44:58 -0300

    Update main.py

--- find_icon_in_screen/main.py
+++ find_icon_in_screen/main.py
@@ -1,27 +1,27 @@
 import pyautogui
 from pywinauto.keyboard import send_keys
 import time
+
 count = 0
-repeticoes = 20
+repeticoes = 999999999
 
 while (True):
     if (count <= repeticoes):
-        icon_pos = pyautogui.locateOnScreen('arrow.png')
-        if icon_pos:
-            print(icon_pos)
-            x = icon_pos.left+3
-            y = icon_pos.top+3
-            pyautogui.click(x, y)
-            send_keys("{DOWN}")
-            send_keys("{DOWN}")
-            send_keys("{DOWN}")
-            send_keys("{DOWN}")
+        z=0;
+        while (z<=7):
+            icon_pos = pyautogui.locateOnScreen('arrow.png')
+            if icon_pos:
+                print(icon_pos)
+                x = icon_pos.left+5
+                y = icon_pos.top+5
+                pyautogui.click(x, y)
+                time.sleep(0.5)
+            z = z + 1
         else:
-            send_keys("{DOWN}")
-            send_keys("{DOWN}")
-            send_keys("{DOWN}")
-            send_keys("{DOWN}")
+            send_keys("{PGDN}")
+            send_keys("{UP}")
+            send_keys("{UP}")
     else:
-        exit()    
-        
+        exit()
+          
     count = count + 1
